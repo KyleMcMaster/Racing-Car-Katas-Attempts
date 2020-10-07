8df970fae70371a77d376888218b85254a084b5e
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2020-10-07 13:01:29 -0400

    feat(ticket-dispenser): add domain event and dbcontext code

---
+++ CSharp/TDDMicroExercises/Features/TicketDispenser/TicketContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TDDMicroExercises.Features.TicketDispenser
+{
+    public class TicketContext : DbContext
+    {
+        public TicketContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TurnTicket> Tickets { get; set; }
+    }
+}
---
+++ CSharp/TDDMicroExercises/Features/TicketDispenser/TicketDispenseEvent.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TDDMicroExercises.Features.TicketDispenser
+{
+    public class TicketDispenseEvent : INotification
+    {
+        public TicketDispenseEvent(int turnNumber)
+        {
+            TurnNumber = turnNumber;
+        }
+
+        public int TurnNumber { get; }
+    }
+
+    public class UniqueNumberHandler : INotificationHandler<TicketDispenseEvent>
+    {
+        private readonly TicketContext ticketContext;
+
+        public UniqueNumberHandler(TicketContext ticketContext)
+        {
+            this.ticketContext = ticketContext;
+        }
+
+        public async Task Handle(TicketDispenseEvent notification, CancellationToken cancellationToken)
+        {
+            var duplicateItem = await ticketContext.Tickets
+                .FirstOrDefaultAsync(t => t.TurnNumber == notification.TurnNumber);
+
+            if (duplicateItem != null)
+            {
+                throw new Exception("Duplicate number not allowed");
+            }
+        }
+    }
+
+    public class TicketDispenseLoggingHandler : INotificationHandler<TicketDispenseEvent>
+    {
+        private readonly ILogger<TicketDispenseEvent> logger;
+
+        public TicketDispenseLoggingHandler(ILogger<TicketDispenseEvent> logger)
+        {
+            this.logger = logger;
+        }
+
+        public async Task Handle(TicketDispenseEvent notification, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"Ticket number {notification.TurnNumber} generated");
+        }
+    }
+}
